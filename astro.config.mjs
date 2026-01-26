84c2fbd839d3242c833dec0b4d449a5bf560109a
Edwin Castro <edwin.castro@agenciamatch.cl>
2026-01-26 14:36:33 -0500

    Configurando deploy automático

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,6 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
   // ¡No pongas nada de tailwind aquí por ahora!
   // Astro verá el archivo postcss.config.mjs automáticamente.
+  site: "https://tu-usuario.github.io",
+  base: "hielo-pro",
 });
