3d84ca481b4ee65f563a3b12bda737bc8d7aed99
Edwin Castro <edwin.castro@agenciamatch.cl>
2026-01-26 17:28:12 -0500

    feat: fix header and add responsive :sparkles:

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ export default defineConfig({
   // ¡No pongas nada de tailwind aquí por ahora!
   // Astro verá el archivo postcss.config.mjs automáticamente.
   site: "https://andreseag.github.io",
-  // base: "/macris-hielo",
+  base: "/macris-hielo",
   output: "static", // Forzamos a que sea estático
 });
